d2175cd4543da01a208ad54d56c14cee7e5b85e7
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-02-17 21:49:18 +0900

    fix build script

--- build.ts
+++ build.ts
@@ -1,2 +1,9 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.11.23/mod.js";
 console.log(esbuild.version);
+
+await esbuild.build({
+    entryPoints: ['./src/index.ts'],
+    bundle: true,
+    outfile: './build/kagura.js',
+});
+Deno.exit();
