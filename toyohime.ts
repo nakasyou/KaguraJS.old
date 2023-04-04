922069bd15efa8b8b24547d0d032e5586683a9e8
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-04 15:24:21 +0900

    Update toyohime.ts

--- toyohime.ts
+++ toyohime.ts
@@ -1,5 +1,17 @@
 import { Toyohime } from "https://deno.land/x/estoyohime@v0.1.2/mod.ts";
 
+const deps = {
+  ["test","v0.1.0","https://test.com","MIT license"]
+}
+let footer = "";
+for(const dep of deps){
+  footer+=`/*
+ * ${dep[0]} ${dep[1]}
+ * ${dep[2]}
+ * ${dep[3]}
+ */
+`
+}
 export default new Toyohime({
   src: "./src/index.ts",
   esmDist: "./dist/esm.js",
@@ -17,4 +29,15 @@ export default new Toyohime({
   importmapPath: "./import_map.json",
   tsconfigPath: "./tsconfig.json",
   globalName: "kagura",
+  banner: {
+    js: `/* 
+ * KaguraJS
+ * MIT Licensed
+ * Copyright (c) 2023 nakasyou.
+ * https://github.com/nakasyou/KaguraJS
+ */`,
+  },
+  footer: {
+    js: footer,
+  }
 });
